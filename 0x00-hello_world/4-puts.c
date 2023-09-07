a667e4fade2c7f79c7160b3a77f45ad6d66830df
anes hamidi <aneshamididev@gmail.com>
2023-09-07 14:04:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Description : A c programe print with puts function
-* Return : Always return 0 (success)
-*/
+ * main - Entry point
+ * Description: c programe print with puts function
+ * Return: Always return 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
