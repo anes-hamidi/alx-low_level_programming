555119d6c40a2356ecb6c771ac24a8a7558d344b
anes hamidi <aneshamididev@gmail.com>
2023-09-07 13:14:04 -0700

    betty doc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+* main - Entry point
+* Description : A c programe print with puts function
+* Return : Always return 0 (success)
+*/
 int main(void)
 {
-/* print Programming is like building a multilingual puzzle with puts */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
