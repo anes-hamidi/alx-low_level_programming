1b9961a9e01496291f50b27d7699fbd220579e86
anes hamidi <aneshamididev@gmail.com>
2023-09-07 12:32:09 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+"""
+print Programming is like building a multilingual puzzle with puts function
+"""
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
