6be739e05f4f03c099f25356df3cbd88fbd3aa95
anes hamidi <aneshamididev@gmail.com>
2023-09-07 12:41:32 -0700

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@ int main(void)
   /*
    *print Programming is like building a multilingual puzzle with puts function
   */
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
