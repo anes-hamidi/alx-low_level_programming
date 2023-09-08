17df8ee360b0c866530fd5dd0bf3e5d6f4e6b455
anes hamidi <aneshamididev@gmail.com>
2023-09-08 12:17:37 -0700

    combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ * Description: prints all possible combinations of single-digit numbers
+ * Return: Always 0.
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 10; x++)
+{
+putchar((x % 10) + '0');
+if (x == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+return (0);
+}
