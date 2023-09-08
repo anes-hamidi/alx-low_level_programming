db002c2e9eb915437e015225d92fceb20dae1b1d
anes hamidi <aneshamididev@gmail.com>
2023-09-08 07:09:16 -0700

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,11 +4,10 @@
  * Discription: print alphabet expect q and a
  * Return: Always  0  (success)
  */
-int main()
+int main(void)
 {
 char l;
 for (l = 'a'; l <= 'z'; l++)
 if (l != 'e' && l != 'q')
 putchar(l);
-
 }
