44be25f4ef7d6b1dc2dbfe90f5d33730673b6463
anes hamidi <aneshamididev@gmail.com>
2023-09-08 07:04:05 -0700

    print alphabet expect q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription: print alphabet expect q and a
+ * Return: Always  0  (success)
+ */
+int main(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+if (l != 'e' && l != 'q')
+putchar(l);
+}
