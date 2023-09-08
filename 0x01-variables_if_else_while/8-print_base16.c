558c984b98ff84470bfdada33f7859986a25bfb0
anes hamidi <aneshamididev@gmail.com>
2023-09-08 12:02:19 -0700

    print numbers base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int b;
+char c;
+for (b = '1'; b <= 10; b++)
+putchar((b % 10) + '0');
+for (c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
