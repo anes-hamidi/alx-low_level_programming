b8e0703962548a23aed218e452b5d5612634e540
anes hamidi <aneshamididev@gmail.com>
2023-09-12 16:08:10 -0700

    print the last degit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @val: integer value
+ * Return: value
+ */
+int print_last_digit(int val)
+{
+int i = (val % 10);
+if (i < 0)
+{
+i *= -1;
+}
+_putchar(i + '0');
+return (i);
+}
