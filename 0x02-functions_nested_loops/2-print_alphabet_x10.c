db5eb7970a1b59e75b79da5f3fc3f84f264c65c4
anes hamidi <aneshamididev@gmail.com>
2023-09-12 11:59:06 -0700

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: function for printing 10 file of alphabet
+ * Return: Always 0 (success) 
+ */
+void print_alphabet_x10(void)
+{
+int i; 
+i = 0;
+while (i < 10) {
+char a;
+for(a = 'a'; a <= 'z'; a++) {
+putchar(a);
+}
+putchar('\n');
+i++;
+}
+}
