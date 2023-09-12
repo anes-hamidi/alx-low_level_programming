9c82104c7c469e4a6b800fdf86cefee9284cc0c8
anes hamidi <aneshamididev@gmail.com>
2023-09-12 15:58:37 -0700

    compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _abs - Entry point
+ * @val: data type
+ * Return: value.
+ */
+int _abs(int val)
+{
+if (val >= 0)
+{
+return (val);
+}
+else
+{
+return (-val);
+}
+
+}
