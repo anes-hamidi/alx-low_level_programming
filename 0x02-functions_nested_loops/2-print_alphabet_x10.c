ec2a4318f43a9bbca21d12def8d7010f3e0f0200
anes hamidi <aneshamididev@gmail.com>
2023-09-12 14:29:20 -0700

    2fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,17 @@
 /**
  * print_alphabet_x10 - Entry point
  * Description: function for printing 10 file of alphabet
- * Return: Always 0 (success) 
+ * Return: Always 0 (success)
  */
 void print_alphabet_x10(void)
 {
-int i; 
+int i;
 i = 0;
-while (i < 10) {
+while (i < 10)
+{
 char a;
-for(a = 'a'; a <= 'z'; a++) {
+for (a = 'a'; a <= 'z'; a++)
+{
 _putchar(a);
 }
 _putchar('\n');
