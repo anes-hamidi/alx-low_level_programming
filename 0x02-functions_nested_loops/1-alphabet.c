00bbe5dc56a9e07a6b1216071c0cdab96e15fa07
anes hamidi <aneshamididev@gmail.com>
2023-09-12 11:18:12 -0700

    print all alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,22 @@
 #include "main.h"
-
+/**
+ * function - print_alphabet
+ * Description: print alphabet lowercase function
+ * Return: Always 0 (success)
+ */
 void print_alphabet(void)
+/* function for print all alphabet folowed by new line */
 {
+/* inisialize index variable */
 int i;
-for(i = 'a'; i <= 'z'; i++)
+/* loop over all alphabet character*/
+for (i = 'a'; i <= 'z'; i++)
 {
+/* print all character */
 _putchar(i);
 }
-putchar('\n');
+/* print new line */
+_putchar('\n');
+/* return: Alwayse 0 (success) */
 return (0);
 }
