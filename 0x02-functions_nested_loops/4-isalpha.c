49946b6a697de7785c50d990290aab968f8f7519
anes hamidi <aneshamididev@gmail.com>
2023-09-12 15:31:18 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,5 @@
-#include "main.h"
 
+#include "main.h"
 /**
  * _isalpha - Entry point
  * @c: character
@@ -7,7 +7,7 @@
  */
 int _isalpha(int c)
 {
-if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+if ((c >= 'a') && (c <= 'z') || (c >= 'A)' && (c <= 'Z'))
 {
 return (1);
 }
