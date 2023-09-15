3961db95ffb28e4283863e7433c0ce4431aade01
anes hamidi <aneshamididev@gmail.com>
2023-09-15 03:23:00 -0700

    fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,8 +3,7 @@
  * mul - multiplies two integer
  * @a: number 1
  * @b: number 2
- * @result: result of function
- * Return: return result of miltiplies two integer
+ * Return: result of miltiplies two integer
  */
 int mul(int a, int b)
 {
