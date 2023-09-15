f402e6add25f05e196fc22f27fc4ac5345ce6d2a
anes hamidi <aneshamididev@gmail.com>
2023-09-15 03:38:06 -0700

    fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
 #include <stdio.h>
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
