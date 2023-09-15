cc960ff3af27c7d8a128de01046c9608146ad57f
anes hamidi <aneshamididev@gmail.com>
2023-09-15 03:56:27 -0700

    print numbers from 1 to 9 and skip 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - desplay numbers from 0 to 9 and skip 2 and 4
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+for (int i = 0; i <= 9; i++)
+{
+if ((i == 2) || (i == 4))
+continue;
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
