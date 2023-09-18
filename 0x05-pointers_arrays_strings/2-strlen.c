36a18c582f2af4befe58c4af78336833cf877b07
anes hamidi <aneshamididev@gmail.com>
2023-09-18 08:05:23 -0700

    string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,11 @@
  * _strlen - function for returns the length of a string
  * Description: calculate length of a string
  * @s: pointer to a char variable
- * @i: counter
  * Return: counter
  */
 int _strlen(char *s)
 {
-int i;
+int i = 0;
 while (*s != '\0')
 {
 i++;
