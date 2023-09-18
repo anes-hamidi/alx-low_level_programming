00a15b39a0bc8ae9adfe66409e7b87189131d313
anes hamidi <aneshamididev@gmail.com>
2023-09-18 09:54:52 -0700

    revers string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,20 @@
+#include "main.h"
 /**
- * rev_string - 
- * Description: 
- * @c:
+ * rev_string - revers string
+ * Description: function for reversing string
+ * @s: pointer to string
  * Return: Always 0 (success)
  */
+void rev_string(char *s)
+{
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+for (int i = len - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
