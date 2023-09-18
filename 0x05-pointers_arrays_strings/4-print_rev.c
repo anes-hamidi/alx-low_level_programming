4af71ddf51fefb7369683ccd2e0c26a5a8ae045b
anes hamidi <aneshamididev@gmail.com>
2023-09-18 09:28:29 -0700

    revers string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,9 @@
 void print_rev(char *s) {
 int len = 0;
 int i;
-while (s[len] != '\0') {
+while (s[len] != '\0') 
 len++; 
-}
-for (i = len - 1; i >= 0; i--) {
+for (i = len - 1; i >= 0; i--) 
 _putchar( s[i]); 
-}
 _putchar('\n');
 }
