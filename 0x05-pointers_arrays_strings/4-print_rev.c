5237326be881d6234d64181bd896da16025edbc4
anes hamidi <aneshamididev@gmail.com>
2023-09-18 09:30:39 -0700

    revers string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,12 +4,13 @@
  * Description: revers string
  * @s: pointer to a string variable
  */
-void print_rev(char *s) {
+void print_rev(char *s)
+{
 int len = 0;
 int i;
 while (s[len] != '\0')
 len++;
 for (i = len - 1; i >= 0; i--)
-_putchar(s[i]); 
+_putchar(s[i]);
 _putchar('\n');
 }
