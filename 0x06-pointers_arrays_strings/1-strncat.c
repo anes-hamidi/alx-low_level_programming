2ffa5b30f3e18869374997699de00a480c9b400e
anes hamidi <aneshamididev@gmail.com>
2023-09-20 10:44:52 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncat - 
+ * @dest:
+ * @src:
+ * @n:
+ * Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int len = 0;
+int i;
+while (dest[len] != '\0')
+{
+len++;
+}
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[len + 1] = src[i];
+}
+dest[len + 1] = '\0';
+return (dest);
+}
