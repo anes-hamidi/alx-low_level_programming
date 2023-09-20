08d1bc4ef839c0a46191ef5311425ff8d36323d9
anes hamidi <aneshamididev@gmail.com>
2023-09-20 14:49:50 -0700

    fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,7 +6,7 @@
  * @n: integer
  * Return: (dest)
 */
-char *strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 int len = 0;
 while (len < n && src[len] != '\0')
