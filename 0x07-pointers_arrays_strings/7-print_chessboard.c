beff6bdc56719de6fc7c8948a46f39b7fc7cd750
anes hamidi <aneshamididev@gmail.com>
2023-09-25 14:13:25 -0700

    fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_chessboard - bitboard
- * @a: pointer to array
- * Return: Always 0 (success)
- * 
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
  */
 
 void print_chessboard(char (*a)[8])
